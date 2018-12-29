0a0b67b21af97d5d49ca0794aa4f4d42b8231824
xihe <citype124@gmail.com>
2018-12-29 14:28:58 +0800

    add: 完成用户详细信息获取接口

--- src/main/java/com/mmall/controller/UserController.java
+++ src/main/java/com/mmall/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mmall.controller;
 
 import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
@@ -122,6 +123,21 @@ public class UserController {
             session.setAttribute(Const.CURRENT_USER, response.getData());
         }
         return response;
+
+    }
+
+    /**
+     * 获取用户的详细信息
+     */
+    @RequestMapping(value = "get_information.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> get_information(HttpSession session) {
+        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
+        if (currentUser == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登录需要强制登录 status = 10");
+
+        }
+        return iUserService.getInformation(currentUser.getId());
 
     }
 
