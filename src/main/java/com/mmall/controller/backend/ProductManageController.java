f316126ad22029dda97ab696eb55888fb0bdb492
xihe <citype124@gmail.com>
2019-01-02 15:57:53 +0800

    finish: 新增产品接口

---
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -0,0 +1,41 @@
+package com.mmall.controller.backend;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+import com.mmall.pojo.User;
+import com.mmall.service.IProductService;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/manage/product")
+public class ProductManageController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @Autowired
+    private IProductService iProductService;
+
+    @RequestMapping("save.do")
+    @ResponseBody
+    public ServerResponse productSave(HttpSession session, Product product) {
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iProductService.saveOrUpdateProduct(product);
+
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+}
